dca4cf8892307f797e654c3c63ae58fe73e7fa00
knosseir <kimo.nosseir@gmail.com>
2017-11-12 10:20:56 -0800

    Implement Tinder-style card view

--- app/src/main/java/com/classy/hoth3/classy/MainActivity.java
+++ app/src/main/java/com/classy/hoth3/classy/MainActivity.java
@@ -2,6 +2,13 @@ package com.classy.hoth3.classy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.daprlabs.aaron.swipedeck.SwipeDeck;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,60 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final SwipeDeck cardStack = (SwipeDeck) findViewById(R.id.swipe_deck);
+
+        final ArrayList<String> testData = new ArrayList<>();
+        testData.add("0");
+        testData.add("1");
+        testData.add("2");
+        testData.add("3");
+        testData.add("4");
+
+        final SwipeDeckAdapter adapter = new SwipeDeckAdapter(testData, this);
+        if(cardStack != null){
+            cardStack.setAdapter(adapter);
+        }
+        cardStack.setCallback(new SwipeDeck.SwipeDeckCallback() {
+            @Override
+            public void cardSwipedLeft(long positionInAdapter) {
+                Log.i("MainActivity", "card was swiped left, position in adapter: " + positionInAdapter);
+            }
+
+            @Override
+            public void cardSwipedRight(long positionInAdapter) {
+                Log.i("MainActivity", "card was swiped right, position in adapter: " + positionInAdapter);
+
+            }
+        });
+
+        cardStack.setLeftImage(R.id.left_image);
+        cardStack.setRightImage(R.id.right_image);
+
+        //example of buttons triggering events on the deck
+        Button btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cardStack.swipeTopCardLeft(180);
+            }
+        });
+        Button btn2 = (Button) findViewById(R.id.button2);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cardStack.swipeTopCardRight(180);
+            }
+        });
+
+        Button btn3 = (Button) findViewById(R.id.button3);
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                testData.add("a sample string.");
+                adapter.notifyDataSetChanged();
+            }
+        });
+
     }
 }
