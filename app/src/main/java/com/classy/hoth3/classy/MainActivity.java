c1b91caebb46d44e0b00c8e8f30ec557a306f121
Rajiv Anisetti <rajiv.anisetti@gmail.com>
2017-11-12 15:52:08 -0800

    More splash stuff

--- app/src/main/java/com/classy/hoth3/classy/MainActivity.java
+++ app/src/main/java/com/classy/hoth3/classy/MainActivity.java
@@ -2,6 +2,7 @@ package com.classy.hoth3.classy;
 
 import android.content.Intent;
 import android.os.AsyncTask;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -20,12 +21,26 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static int SPLASH_TIME_OUT = 4000;
+
     String subject, url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                Intent homeIntent = new Intent(MainActivity.this, SplashActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+
+        }, SPLASH_TIME_OUT);
+
+        //setContentView(R.layout.activity_main);
+
 
         final SwipeDeck cardStack = (SwipeDeck) findViewById(R.id.swipe_deck);
 
