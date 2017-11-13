c9b2118ed649190addcf668b52df370c595b7a7f
Rajiv Anisetti <rajiv.anisetti@gmail.com>
2017-11-12 16:47:02 -0800

    Finally fixed splash screen

--- app/src/main/java/com/classy/hoth3/classy/SplashActivity.java
+++ app/src/main/java/com/classy/hoth3/classy/SplashActivity.java
@@ -1,13 +1,33 @@
 package com.classy.hoth3.classy;
 
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private final int SPLASH_TIME = 1000;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashActivity.this,MainActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+            }
+
+        }, SPLASH_TIME);
+
     }
 }
